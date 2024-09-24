b8d8e59cba0a4c00217f9c04775f3a6a39d6a0ca
Shreya1199 <123320240+Shreya1199@users.noreply.github.com>
2024-09-24 19:51:57 +0530

    Update TelemetryBotPOC_V5.py

--- TelemetryBotPOC_V5.py
+++ TelemetryBotPOC_V5.py
@@ -13,8 +13,6 @@ from IPython.display import display
 from IPython.display import Markdown
 import os
 import pandas as pd
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
@@ -22,7 +20,6 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 import streamlit as st
 import pandasql as ps
 import json
